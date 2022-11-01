53e3380b073978a319a4b6d0b7429cb59d7bd229
Brandon Gray <brandonjoe88@hotmail.com>
2022-11-01 09:41:42 -0600

    feat: election races added

--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -6,7 +6,8 @@ require './lib/election'
 describe Election do
   before(:each) do
     @election = Election.new('2022')
-    @race1 = Race.new("Texas Governor")
+    @race1 = Race.new("Virginia District 4 Representative")
+    @race2 = Race.new("Texas Governor")
   end
 
   describe '#initialization' do
@@ -20,6 +21,15 @@ describe Election do
 
     it 'has no race upon creation' do
       expect(@election.races).to eq([])
+    end
+  end
+
+  describe '#add_race' do
+    it 'adds races to the election collection' do
+      @election.add_race(@race1)
+      @election.add_race(@race2)
+
+      expect(@election.races).to eq([@race1, @race2])
     end
   end
 end
