f24f7ee417d34fb5407fe0edad8be96654464a5b
Brandon Gray <brandonjoe88@hotmail.com>
2022-11-01 09:50:39 -0600

    feat: election vote counts create

--- lib/election.rb
+++ lib/election.rb
@@ -11,4 +11,12 @@ class Election
   def add_race(race)
     @races.push(race)
   end
+
+  def candidates
+    @races.map {||}
+  end
+
+  def vote_counts
+    @races.map {|race| p race}
+  end
 end
--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -32,4 +32,25 @@ describe Election do
       expect(@election.races).to eq([@race1, @race2])
     end
   end
+
+  describe '#vote_counts' do
+    it 'keeps a tally of votes' do
+      @election.add_race(@race1)
+      @election.add_race(@race2)
+
+      candidate1 = @race1.register_candidate!({name: "Diana D", party: :democrat})
+      candidate2 = @race1.register_candidate!({name: "Roberto R", party: :republican})
+      candidate3 = @race2.register_candidate!({name: "Diego D", party: :democrat})
+      candidate4 = @race2.register_candidate!({name: "Rita R", party: :republican})
+      candidate5 = @race2.register_candidate!({name: "Ida I", party: :independent})
+
+      4.times {candidate1.vote_for!}
+      1.times {candidate2.vote_for!}
+      10.times {candidate3.vote_for!}
+      6.times {candidate4.vote_for!}
+      6.times {candidate5.vote_for!}
+
+      expect(@election.vote_counts).to eq("")
+    end
+  end
 end
